2672db91f2440c8747a7c8466635ba228848dadf
angibarbu <angibarbu@gmail.com>
2023-03-18 15:22:25 +0200

    Am adaugat baza proiectului

---
+++ blog/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+posts = [
+    {
+        'author': 'Angi',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'March 18, 2023'
+    },
+    {
+        'author': 'Jane',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'March 19, 2023'
+    }
+
+]
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
